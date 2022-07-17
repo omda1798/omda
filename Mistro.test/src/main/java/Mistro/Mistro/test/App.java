871b99b6f7f813e3012e0f979256cb873bef8716
unknown <ahmed.emad172811@gmail.com>
2022-07-17 15:59:46 +0200

    commit

---
+++ Mistro.test/src/main/java/Mistro/Mistro/test/App.java
@@ -0,0 +1,13 @@
+package Mistro.Mistro.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
